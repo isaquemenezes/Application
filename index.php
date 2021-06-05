699a4b7ac7f3b4ab44011a62d9534055c931d3e5
Isaque Menezes da Silva <isaque-menezes@hotmail.com>
2021-06-04 21:36:17 -0300

    codigo fonte

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?php
-    // require_once 'lib/livro/core/Classloader.php';
+    date_default_timezone_set('America/Sao_Paulo');
 
-    /*
-        php -v
-        PHP 7.2.6 (cli) (built: May 23 2018 20:51:04) ( ZTS MSVC15 (Visual C++ 2017) x86)
-        Copyright (c) 1997-2018 The PHP Group
-        Zend Engine v3.2.0, Copyright (c) 1998-2018 Zend Technologies
-    */
-    
-    
-    header("Content-Type: text/html; charset=utf-8"); //Cabeçalho
-    date_default_timezone_set("America/Belem");       //Fuso horário Local
-    // error_reporting(0);   //Ocultando erros do sistema, do users           
+    if (version_compare(PHP_VERSION, '7.0.0') == -1){
+        die('A versão mínima para rodar esta aplicação é: 7.0.0');
+    }
 
-    include ("Config/config.php");               //Arquivo de configuração
-    // include (DIRREQ."library/vendor/autoload.php");  //Arquivo de autoload
-    // include (DIRREQ."helpers/variables.php");    //variáveis de imput 
-    
-    $load = require 'library/vendor/autoload.php';
-    $load->register();
-    
- 
-    $dispatch = new Classes\ClassDispatch();  //objeto para configuração para localhost(index da views)
-    include ($dispatch ->getInclusao());       //dispatch para index da views
-    //include ($dispatch ->getInclusaoTemp());       //dispatch para index_temp(page temporária)
-     
-    
-
-
-
-
-
-
-    
+    //Lib Loader
+    require_once 'Lib/Livro/ClassLoader.php';
+    $al = new Livro\Core\ClassLoader;
+    $al->addNamespace('Livro', 'Lib/Livro');
+    $al->register();
